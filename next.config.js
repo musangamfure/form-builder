c757a7d74718f8baa96403982453056eef149031
Musangamfura Emmanuel <musangamfure@gmail.com>
2024-01-16 21:37:31 +0200

    Install next js and other deps

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'bcryptjs'],
+  },
+};
+
+module.exports = nextConfig;
